bc87c7645388bfb0013ee0cfc18b653870847eab
Charles George <41029825+87designer@users.noreply.github.com>
2022-03-15 20:13:02 -0400

    Getting Closer
    Updated the code. Now correctly solving half the tests. Need to account for one more aspect of the problem and I think it will be good to go.

--- Don't give me five! Really!/solution.py
+++ Don't give me five! Really!/solution.py
@@ -1,7 +1,20 @@
-import math
-
 # DEVELOPMENT ONGOING
 def dont_give_me_five(start, end):
-    count = end - start
-    fives = math.floor(count / 10)
-    return count - fives
+    # Try a Positional Notation loop
+    array_length = (end - start)
+    if 5 in [int(d) for d in str(abs(start))] or 5 in [int(d) for d in str(abs(end))]:
+        array_length -= 1
+    digits = [int(d) for d in str(array_length)]
+    print(f'Count = {array_length}')
+    print(f'Digits = {len(digits)}')
+    fives = 0
+    if start >= -4 and end <= 4:
+        return 0
+    if array_length <= 10 and array_length >= 5:
+        return 1
+    for i in range(len(digits)-1,-1,-1):
+        print(i)
+        print(digits[::-1][i] * (10 ** i))
+        fives += digits[::-1][i]*(10**i-9**i)
+    print(f'Fives = {fives}')
+    return array_length - fives
